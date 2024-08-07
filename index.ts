1a7fe7a5f80fc9a12238eed7c9e85da56a91b188
Kieran Klukas <92754843+kcoderhtml@users.noreply.github.com>
2024-08-06 20:51:44 -0400

    chore: Add emojis.json to .gitignore and fetch and save emojis from Slack API

--- index.ts
+++ index.ts
@@ -1,1 +1,21 @@
-console.log("Hello via Bun!");
+interface EmojiResponse {
+    ok: boolean;
+    emoji: {
+        [key: string]: string;
+    };
+}
+
+const response: EmojiResponse = await (await fetch(
+    "https://slack.com/api/emoji.list",
+    {
+        headers: {
+            "Authorization": "Bearer " + process.env.SLACKTOKEN
+        }
+    }
+)).json();
+
+
+console.log("number of emojis gotten",Object.keys(response.emoji).length)
+
+// save to file with bun
+Bun.write("emojis.json", JSON.stringify(response.emoji))
